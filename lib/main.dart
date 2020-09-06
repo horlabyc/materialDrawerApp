a5db99be44687e16ef407017a96e15fe97b18bbb
Sulaiman <sulaimain@kobo360.com>
2020-09-06 21:56:35 +0100

    Created drawer items

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.lime),
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
       home: MainScreen(),
     );
   }
@@ -19,6 +20,50 @@ class MainScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Drawer App'),
+        centerTitle: true,
+        elevation: defaultTargetPlatform == TargetPlatform.iOS ? 5.0 : 0.0,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text('Jamaaldeen Akolade'),
+              accountEmail: Text('jamaal@gmail.com'),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Text('JA'),
+              ),
+              otherAccountsPictures: [
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Text('D'),
+                ),
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Text('B'),
+                ),
+              ],
+            ),
+            ListTile(
+              title: Text('Page One'),
+              trailing: Icon(Icons.arrow_upward),
+            ),
+            ListTile(
+              title: Text('Page Two'),
+              trailing: Icon(Icons.arrow_downward),
+            ),
+            Divider(),
+            ListTile(
+              title: Text('Closee'),
+              trailing: Icon(Icons.close),
+            )
+          ],
+        ),
+      ),
+      body: Container(
+        child: Center(
+          child: Text('Home page'),
+        ),
       ),
     );
   }
